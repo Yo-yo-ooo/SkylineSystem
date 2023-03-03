7724cdfbc79f57a10cb91289ef1f9559f66bee4c
yoyo102421@163.com <yoyo102421@163.com>
2023-03-03 20:37:32 +0800

    a

--- kernel/kernel/src/memory/new.hpp
+++ kernel/kernel/src/memory/new.hpp
@@ -5,4 +5,4 @@ extern const char* _X__file__;
 extern const char* _X__func__;
 extern size_t _X__line__;
 #define new (_X__func__=__PRETTY_FUNCTION__,_X__file__=__FILE__,_X__line__=__LINE__) && 0 ? NULL : new
-
+#define delete (_X__func__=__PRETTY_FUNCTION__,_X__file__=__FILE__,_X__line__=__LINE__) && 0 ? NULL : delete
--- kernel/kernel/src/memory/new2.cpp
+++ kernel/kernel/src/memory/new2.cpp
@@ -5,3 +5,7 @@ void* _Ymalloc(size_t size, const char* func, const char* file, int line)
 {
     return _Xmalloc(size, func, file, line);
 }
+
+void _Yfree(void* address, const char* func, const char* file, int line){
+    _Xfree(address,func,  file, line);
+}
