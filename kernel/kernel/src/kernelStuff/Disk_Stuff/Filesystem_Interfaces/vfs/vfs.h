7c5e1ed8cb79e405a353f9262710699d985a3f81
yoyo102421@163.com <yoyo102421@163.com>
2023-03-26 10:27:06 +0800

    a

--- kernel/kernel/src/kernelStuff/Disk_Stuff/Filesystem_Interfaces/vfs/vfs.h
+++ kernel/kernel/src/kernelStuff/Disk_Stuff/Filesystem_Interfaces/vfs/vfs.h
@@ -6,12 +6,19 @@ struct VitualFileSystemOPS{
     int (*read)(int fd, void * buf, size_t count);
     int (*write)(int fd, const void * buf, size_t count);
     int (*create)(const char * pathname, int mode);
-    int (*mkdir)(const char *pathname, int mode);
+    int (*mkdir)(const char *pathname);
     int (*rmdir)(const char *pathname);
     int (*rename)(const char *old_filename, const char *new_filename);
     int (*mount)(char* dev_name, char* mount_point, char* fs_name);
     int (*unmount)(char* mount_point);
+    int (*lseek)(int fd, int offset, int whence);
+    int (*close)(int fd);
+    int (*sync)(void);
+    int (*chdir)(const char *path);
+    int (*fchdir)(int fd);
 };
+
+#define VFS_REG(Pos,function) .Pos = function 
 
 #define VFS_MAX 100
 
