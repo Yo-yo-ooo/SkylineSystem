5d402c4f52b4fb9dcf5fabbfcf1c8ccc98f1934e
Yoyo <98507772+Yo-yo-ooo@users.noreply.github.com>
2025-12-19 10:33:24 +0800

    add welcome

--- gdef.mk
+++ gdef.mk
@@ -1,5 +1,5 @@
 # Architecture to build for SkylineSystem
 # You must set in thest architecture: x86_64, aarch64, riscv64, loongarch64
 
-BUILD_ARCH = x86_64
+BUILD_ARCH = aarch64
 PROGRAM_IMAGE_NAME = disk
--- kernel/src/arch/aarch64/init.cpp
+++ kernel/src/arch/aarch64/init.cpp
@@ -1,5 +1,8 @@
+#include <klib/kprintf.h>
+#include <arch/pinc.h>
+
 bool __aarch64_have_lse_atomics;
 
 void aarch64_init(void){
-
+    WELCOME_AARCH64
 }
