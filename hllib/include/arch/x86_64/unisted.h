8f16272d9a0b248b5e0883f8ddc503a575cdca60
Yoyo <98507772+Yo-yo-ooo@users.noreply.github.com>
2025-12-19 09:56:33 +0800

    IDK Why

--- hllib/include/arch/x86_64/unisted.h
+++ hllib/include/arch/x86_64/unisted.h
@@ -5,6 +5,14 @@
 #include <arch/x86_64/types.h>
 #include <arch/x86_64/unsiteds.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 ssize_t write(int fd,const void *buf, size_t count);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
--- programs/desktop/main.cpp
+++ programs/desktop/main.cpp
@@ -1,4 +1,5 @@
 #include <unisted.h>
+#include <syscall.h>
 
 extern "C" void phl(void);
 extern "C" void exit(void);
@@ -6,7 +7,15 @@ extern "C" void exit(void);
 //test exit in user prog
 
 int main(){
-    //write(1,"Hello, World!\n",13);
+    /* ssize_t x = write(1,"Hello, World!\n",13); */
+    ssize_t ret;
+    syscall3(
+        __NR_write,
+        (uint64_t)1,
+        (uint64_t)"Hello, World!\n",
+        (uint64_t)13,
+        ret
+    );
     phl();
     phl();
     while(true);
