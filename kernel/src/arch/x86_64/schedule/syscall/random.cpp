01a287af1b4ef4ad61ba078b2adcfb5edd1941c8
Yoyo <98507772+Yo-yo-ooo@users.noreply.github.com>
2026-01-23 19:14:00 +0800

    smol bug fix

--- kernel/src/arch/x86_64/schedule/syscall/random.cpp
+++ kernel/src/arch/x86_64/schedule/syscall/random.cpp
@@ -7,6 +7,7 @@
 #include <klib/rnd.h>
 #include <klib/kio.h>
 #include <arch/x86_64/pit/pit.h>
+#include <arch/x86_64/drivers/hpet/hpet.h>
 
 #define GRND_NONBLOCK	0x0001
 #define GRND_RANDOM	0x0002
@@ -33,7 +34,7 @@ uint64_t sys_getrandom(uint64_t buf, uint64_t size, uint64_t flags,
     uint64_t length = size;
     while (length >= 8) {
         uint64_t value = (uint64_t) ((RND::rand(1337, 0, 0x8FFFFFFF) % 65535)
-                                     * (rdtsc() % 65535));
+                                     * (HPET::GetTimeNS() % 65535));
         *((uint64_t *) (ret_buf)) = value;
         ret_buf += 8;
         length -= 8;
@@ -41,7 +42,7 @@ uint64_t sys_getrandom(uint64_t buf, uint64_t size, uint64_t flags,
 
     if (length > 0) {
         uint64_t value = (uint64_t) ((RND::rand(1337, 0, 0x8FFFFFFF) % 65535)
-                                     * (rdtsc() % 65535));
+                                     * (HPET::GetTimeNS() % 65535));
         __memcpy(ret_buf, &value, length);
     }
     return 0;
