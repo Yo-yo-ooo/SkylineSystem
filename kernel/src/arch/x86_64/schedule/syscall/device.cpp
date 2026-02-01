dd72e05e5e3f70c5f373ab779d612b9a778f9a5a
Yoyo <98507772+Yo-yo-ooo@users.noreply.github.com>
2026-02-01 14:55:27 +0800

    a

--- kernel/src/arch/x86_64/schedule/syscall/device.cpp
+++ kernel/src/arch/x86_64/schedule/syscall/device.cpp
@@ -8,13 +8,18 @@
 
 // IN LIBC This SYSCALL EXPRESS: uint64_t sys_dev_read(DevType dt,uint64_t DevIDX,...)
 uint64_t sys_dev_read(uint64_t DevType,uint64_t DevIDX,\
-uint64_t buffer,uint64_t count,GENERATE_IGN2()){
-    IGNV_2();
-    
+uint64_t buffer,uint64_t count,uint64_t offset,uint64_t ign_0){
+    IGNORE_VALUE(ign_0);
+    return Dev::DeviceRead(DevType,DevIDX,offset,(void*)buffer,count);
 }
 
 uint64_t sys_dev_write(uint64_t DevType,uint64_t DevIDX,\
-uint64_t buffer,uint64_t count,GENERATE_IGN2()){
-    IGNV_2();
+uint64_t buffer,uint64_t count,uint64_t offset,uint64_t ign_0){
+    IGNORE_VALUE(ign_0);
+    return Dev::DeviceWrite(DevType,DevIDX,offset,(void*)buffer,count);
+}
 
+uint64_t sys_dev_mmap(uint64_t DevType,uint64_t DevIDX,
+uint64_t length,uint64_t prot,uint64_t flags,uint64_t offset){
+    
 }
