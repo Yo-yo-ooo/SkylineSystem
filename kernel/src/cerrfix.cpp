86d43428a0d95772b5ea60256b154fa4b21a321e
Yoyo <98507772+Yo-yo-ooo@users.noreply.github.com>
2026-02-16 22:50:45 +0800

    Add GPLv2 declaration in *.cpp

--- kernel/src/cerrfix.cpp
+++ kernel/src/cerrfix.cpp
@@ -1,3 +1,24 @@
+/*
+* SPDX-License-Identifier: GPL-2.0-only
+* File: cerrfix.cpp
+* Copyright (C) 2026 Yo-yo-ooo
+*
+* This file is part of SkylineSystem.
+*
+* SkylineSystem is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+*/
 //These function just for compile (idk why)
 //This function must not be delete
 extern "C" void _Unwind_Resume(void){/*unrealized!*/}
