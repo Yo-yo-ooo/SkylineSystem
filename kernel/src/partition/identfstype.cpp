45c165cf0c66ccbad07d0847dfd7e1ed4285674b
Yoyo <98507772+Yo-yo-ooo@users.noreply.github.com>
2026-06-14 15:15:17 +0800

    Identify FS Type function avail! & Kernel Fast opt

--- kernel/src/partition/identfstype.cpp
+++ kernel/src/partition/identfstype.cpp
@@ -28,87 +28,13 @@
 #include <fs/lwext4/ext4.h>
 
 #include <conf.h>
-/* 
-FS_TYPE_ID IdentifyFSType(uint32_t DriverID,uint32_t PartitionID){
-    
 
-    if(IdentifyFat(DriverID,PartitionID,ENABLE_VIRT_IMAGE).ErrorCode == 0 &&
-        IdentifyFat(DriverID,PartitionID,ENABLE_VIRT_IMAGE).TypeID != PARTITION_TYPE_UNKNOWN)
-        return IdentifyFat(DriverID,PartitionID,ENABLE_VIRT_IMAGE).TypeID;
-    elif(IdentifyExtx(DriverID,PartitionID,ENABLE_VIRT_IMAGE).ErrorCode == 0 &&
-        IdentifyExtx(DriverID,PartitionID,ENABLE_VIRT_IMAGE).TypeID != PARTITION_TYPE_UNKNOWN)
-        return IdentifyExtx(DriverID,PartitionID,ENABLE_VIRT_IMAGE).TypeID;
-    else
-        return PARTITION_TYPE_UNKNOWN;
+FS_TYPE_ID IdentifyFSType(VsDevType DriverType, uint32_t DriverID, uint32_t PartitionID) {
+    FS_TYPE TMP = IdentifyFat(DriverType, DriverID, PartitionID, ENABLE_VIRT_IMAGE);
+    if (TMP.ErrorCode == 0 && TMP.TypeID != PARTITION_TYPE_UNKNOWN) 
+        return TMP.TypeID;
+    TMP = IdentifyExtx(DriverType, DriverID, PartitionID, ENABLE_VIRT_IMAGE);
+    if (TMP.ErrorCode == 0 && TMP.TypeID != PARTITION_TYPE_UNKNOWN)
+        return TMP.TypeID;
+    return PARTITION_TYPE_UNKNOWN;
 }
-
-FF_DESC *fs_desc;
-uint8_t ff_count;
-
-bool FSAllIdentify(){
-#ifdef USE_VIRT_IMAGE
-        fs_desc = (FF_DESC*)kmalloc(1 * sizeof(FF_DESC));
-        ff_count = 1;
-        fs_desc[0].FSType = IdentifyFSType(0,0);
-        fs_desc[0].DriverID = 0;
-        fs_desc[0].PartitionID = 0;
-        fs_desc[0].Prdv = 0;
-        fs_desc[0].PStart = 0;
-        for(uint32_t i = 0;i < Dev::vsdev_list_idx;i++){
-            if(Dev::DevList_[i].type != VsDevType::NSDEV
-            &&  Dev::DevList_[i].type != VsDevType::Undefined){
-                fs_desc[0].PEnd = Dev::DevList_[i].ops.GetMaxSectorCount(Dev::DevList_[i].classp) * 512;
-            }
-        }
-        fs_desc[0].Size = fs_desc[0].PEnd;
-#else
-    fs_desc = (FF_DESC*)kmalloc(10 * sizeof(FF_DESC));
-
-    for(uint32_t i = 0;i < Dev::vsdev_list_idx;i++){
-        if(Dev::DevList_[i].type != VsDevType::NSDEV
-        &&  Dev::DevList_[i].type != VsDevType::Undefined){
-
-            for(uint8_t j = 0;j < GetPartitionCount(i);j++){
-                uint8_t type = IdentifyFSType(i,j);
-                fs_desc[ff_count].FSType = type;
-                fs_desc[ff_count].DriverID = i;
-                fs_desc[ff_count].PartitionID = j;
-                fs_desc[ff_count].Prdv = ff_count;
-                if(GetPartitionStart(i,j,fs_desc[ff_count].PStart) != 0)
-                    return false;
-                if(GetPartitionEnd(i,j,fs_desc[ff_count].PEnd) != 0)
-                    return false;
-                fs_desc[ff_count].Size = fs_desc[ff_count].PEnd - fs_desc[ff_count].PStart;
-                if(ff_count > 10){
-                    fs_desc = (FF_DESC*)krealloc(fs_desc,ff_count * sizeof(FF_DESC));
-                }
-
-                ff_count++;
-            }
-
-        }
-    }
-#endif
-    return true;
-} */
-
-
-/* void FSPrintDesc(){
-    //uint8_t ff_count;
-
-    for(uint32_t i = 0;i < ff_count;i++){
-#define _IDENTIFY_KILN(info) kinfoln("%d",info);
-        _IDENTIFY_KILN(fs_desc[i].FSType)
-        _IDENTIFY_KILN(fs_desc[i].DriverID) 
-        _IDENTIFY_KILN(fs_desc[i].PartitionID )
-        _IDENTIFY_KILN(fs_desc[i].Prdv)
-        _IDENTIFY_KILN(fs_desc[i].PStart)
-        _IDENTIFY_KILN(fs_desc[i].PEnd)
-        _IDENTIFY_KILN(fs_desc[i].Size )
-#undef _IDENTIFY_KILN
-    }
-    return true;
-}
- */
-
-//shutdown func:kfree(fs_desc);
