f0058e3402c4d31b4200495c93d207fb2118a014
Yoyo <98507772+Yo-yo-ooo@users.noreply.github.com>
2026-08-03 20:55:12 +0800

    ADD GOD FILE A COMMENT

--- kernel/src/arch/x86_64/schedule/sched.cpp
+++ kernel/src/arch/x86_64/schedule/sched.cpp
@@ -1,5 +1,6 @@
 // SPDX-FileCopyrightText: 2026 Yo-yo-ooo
 // SPDX-License-Identifier: GPL-2.0-only
+// EEVDF IMPL HERE
 #include <arch/x86_64/allin.h>
 #include <elf/elf.h>
 #include <arch/x86_64/schedule/sched.h>
