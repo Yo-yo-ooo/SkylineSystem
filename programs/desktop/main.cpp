8127fec534955640060798119d1cf08c9b1e4572
Yoyo <98507772+Yo-yo-ooo@users.noreply.github.com>
2026-08-25 15:16:29 +0800

    [OK] SHARED PROC-PROC Page Mapping

--- programs/desktop/main.cpp
+++ programs/desktop/main.cpp
@@ -117,6 +117,7 @@ int main(){
     for(;;); */
     uint64_t pid = sys_load((uint64_t)"/mp/hw2.elf",0,0);
     uint64_t dstPID = sys_getpid();
+    
     sys_pmmapSHARE(dstPID,0,fb.BufferSize,VMM_FLAG_READWRITE,pid,0);
     sys_launch(pid); 
     
